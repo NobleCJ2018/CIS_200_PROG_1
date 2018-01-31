c15136f660560b0daea4311d5f0503c335bfe762
Clell J Noble <NobleCJ2018@users.noreply.github.com>
2018-01-30 19:50:37 -0500

    ADD: LibraryPatron.cs from Prog0

--- CIS_200_Prog_1/LibraryPatron.cs
+++ CIS_200_Prog_1/LibraryPatron.cs
@@ -12,10 +12,75 @@ Section********: CIS 200 01
 */
 
 // FileName: LibraryPatron.cs
+// Class: LibraryPatron [Origin: Prog_0]
 
 namespace CIS_200_Prog_1
 {
     class LibraryPatron
     {
+        private string _patronName; // Name of the patron
+        private string _patronID;   // ID of the patron
+
+        // Precondition:  None
+        // Postcondition: The patron has been initialized with the specified name and ID
+        
+        public LibraryPatron(string name, string id)
+        {
+            PatronName = name;
+            PatronID = id;
+        }
+
+        public string PatronName
+        {
+            // Precondition:  None
+            // Postcondition: The patron's name has been returned
+            get
+            {
+                return _patronName;
+            }
+
+            // Precondition:  None
+            // Postcondition: The patron's name has been set to the specified value
+            set
+            {
+
+                if (string.IsNullOrWhiteSpace(value)) // IsNullOrWhiteSpace includes tests for null, empty, or all whitespace
+                    throw new ArgumentOutOfRangeException($"{nameof(PatronName)}", value,
+                        $"{nameof(PatronName)} must not be null or empty");
+                else
+                    _patronName = value.Trim();
+            }
+        }
+
+        public string PatronID
+        {
+            // Precondition:  None
+            // Postcondition: The patron's ID has been returned
+            get
+            {
+                return _patronID;
+            }
+
+            // Precondition:  None
+            // Postcondition: The patron's ID has been set to the specified value
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value)) // IsNullOrWhiteSpace includes tests for null, empty, or all whitespace
+                    throw new ArgumentOutOfRangeException($"{nameof(PatronID)}", value,
+                        $"{nameof(PatronID)} must not be null or empty");
+                else
+                    _patronID = value.Trim();
+            }
+        }
+
+        // Precondition:  None
+        // Postcondition: A string is returned presenting the libary patron's data on separate lines
+        
+        public override string ToString()
+        {
+            string NL = Environment.NewLine; // NewLine shortcut
+
+            return $"Name: {PatronName}{NL}ID: {PatronID}";
+        }
     }
 }
