a7e16454788591f8ad5de7fd26c650792ea36ded
CJN <noblecj2018@users.noreply.gitub.com>
2018-02-01 11:53:18 -0500

    LibraryItem Add Prop

--- CIS_200_Prog_1/LibraryItem.cs
+++ CIS_200_Prog_1/LibraryItem.cs
@@ -17,5 +17,91 @@ namespace CIS_200_Prog_1
 {
     class LibraryItem
     {
+        private string _title;
+        private string _publisher;
+        private int _copyrightYear;
+        private int _loanPeriod;
+        private string _callNumber;
+
+        public LibraryItem(string title, string publisher, int copyrightYear, int loanPeriod, string callNumber)
+        {
+            
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Title)} Please Enter A Title");
+                }
+                Title = value.Trim();
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, $"{nameof(Publisher)} Please Enter a Publisher");
+                }
+                ///////// Stop 2/1/2018
+                Publisher = value;
+            }
+        }
+
+        public int CopyrightYear
+        {
+            get
+            {
+                return _copyrightYear;
+            }
+
+            set
+            {
+                CopyrightYear = value;
+            }
+        }
+
+        public int LoanPeriod
+        {
+            get
+            {
+                return _loanPeriod;
+            }
+
+            set
+            {
+                LoanPeriod = value;
+            }
+        }
+
+        public string CallNumber
+        {
+            get
+            {
+                return _callNumber;
+            }
+            set
+            {
+                CallNumber = value;
+            }
+        }
+
     }
+
+        
+   
 }
