addeea30dc7e0a4abeb941b78da314066c58cc6a
Clell J Noble <NobleCJ2018@users.noreply.github.com>
2018-02-14 01:22:52 -0500

    L-movie.cs

--- CIS_200_Prog_1/LibraryMediaItem.cs
+++ CIS_200_Prog_1/LibraryMediaItem.cs
@@ -19,6 +19,8 @@ namespace CIS_200_Prog_1
     {
         private double _duration;
 
+        public enum MediaType { DVD, BLURAY, VHS, CD, SACD, VINYL};
+
         public LibraryMediaItem(string theTitle, string thePublisher, int theCopyrightYear, int theLoanPeriod,
                                 string theCallNumber, double duration)
                                 :base(theTitle, thePublisher, theCopyrightYear, theLoanPeriod, theCallNumber)
@@ -43,7 +45,26 @@ namespace CIS_200_Prog_1
             }
         }
 
+
+        
+        
+        public abstract MediaType Medium { get; set; }
+
         public abstract override decimal CalcLateFee(int daysLate);
+
+        public override string ToString()
+        {
+            string NL = Environment.NewLine; // NewLine shortcut
+            string checkedOutBy; // Holds checked out message
+
+            if (IsCheckedOut())
+                checkedOutBy = $"Checked Out By: {NL}{Patron}";
+            else
+                checkedOutBy = "Not Checked Out";
+
+            return $"Title: {Title}{NL}Publisher: {Publisher}{NL}Duration: {Duration}" +
+                $"Copyright: {CopyrightYear}{NL}Call Number: {CallNumber}{NL}{checkedOutBy}";
+        }
 
 
 
--- CIS_200_Prog_1/LibraryMovie.cs
+++ CIS_200_Prog_1/LibraryMovie.cs
@@ -15,7 +15,121 @@ Section********: CIS 200 01
 
 namespace CIS_200_Prog_1
 {
-    public class LibraryMovie
+    public class LibraryMovie : LibraryMediaItem
     {
+        private string _director;
+        private MediaType _medium;
+        private MPAARatings _ratings;
+        public enum MPAARatings { G, PG, PG13, R, NC17, U };
+
+        public LibraryMovie(string theTitle, string thePublisher, int theCopyrightYear, int theLoanPeriod,
+                            string theCallNumber, double theDuration, string theDirector, MediaType theMedium,
+                            MPAARatings theRating)
+                            : base(theTitle, thePublisher, theCopyrightYear, theLoanPeriod,
+                                  theCallNumber, theDuration)
+        {
+
+        }
+
+        public string Director
+        {
+            // Precondition:  None
+            // Postcondition: The director has been returned
+            get
+            {
+                return _director;
+            }
+
+            set
+            {
+                // Precondition:  value must not be null or empty
+                // Postcondition: The director has been set to the specified value
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(Director)}", value, $"{nameof(Director)} please enter a Director");
+                else
+                    _director = value.Trim();
+            }
+        }
+
+        public override MediaType Medium
+        {
+
+            get
+            {
+                return _medium;
+            }
+
+            set
+            {
+                // conditional for DVD, BLURAY, and VHS only???
+                if (value == MediaType.DVD || value == MediaType.BLURAY || value == MediaType.VHS)
+                    _medium = value;
+                else
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(Medium)}", value, $"{nameof(Medium)} please enter a valid medium for a movie");
+                    
+            }
+
+        }
+
+        public MPAARatings Ratings
+        {
+            get
+            {
+                return _ratings;
+            }
+
+            set
+            {
+
+                // conditional for non rating values???
+                if (value == MPAARatings.G || value == MPAARatings.PG || value == MPAARatings.PG13 ||
+                   value == MPAARatings.R || value == MPAARatings.NC17 || value == MPAARatings.U)
+                {
+                    _ratings = value;
+                }
+                else
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(Ratings)}", value, $"{nameof(Ratings)} please enter a valid MPAA rating");
+            }
+        }
+
+
+        public override decimal CalcLateFee(int daysLate)
+        {
+
+            const decimal LATE_FEE_RATE_BLURAY = 1.50m; // Bluray daily rate
+            const decimal LATE_FEE_RATE_DVD_VHS = 1.00m; // DVD/VHS daily rate
+            const decimal MAX_CHARGE_LIMIT = 25.00m; // max limit for all movie types
+            decimal chargeFee;
+            
+            if (this.Medium == MediaType.DVD)
+            {
+                if (daysLate * LATE_FEE_RATE_DVD_VHS >= MAX_CHARGE_LIMIT)
+                {
+                    chargeFee = MAX_CHARGE_LIMIT;
+                }
+                else
+                    chargeFee = daysLate * LATE_FEE_RATE_DVD_VHS;
+            }
+
+            else 
+            {
+                if (daysLate * LATE_FEE_RATE_BLURAY >= MAX_CHARGE_LIMIT)
+                {
+                    chargeFee = MAX_CHARGE_LIMIT;
+                }
+                else
+                    chargeFee = daysLate * LATE_FEE_RATE_BLURAY;
+            }
+
+            return chargeFee;
+
+            
+
+
+            
+        }
     }
 }
