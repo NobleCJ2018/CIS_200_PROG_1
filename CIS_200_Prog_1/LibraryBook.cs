8d060d2e7557fdbb2b69089f1bfda7e9f32d3129
Clell J Noble <NobleCJ2018@users.noreply.github.com>
2018-02-13 21:06:59 -0500

    L-Item.cs + L-Book.cs

--- CIS_200_Prog_1/LibraryBook.cs
+++ CIS_200_Prog_1/LibraryBook.cs
@@ -15,7 +15,71 @@ Section********: CIS 200 01
 
 namespace CIS_200_Prog_1
 {
-    class LibraryBook
+    public class LibraryBook : LibraryItem
     {
+        private string _author;
+
+
+        public LibraryBook(string theTitle, string theAuthor, string thePublisher, int theCopyrightYear, 
+                           int theLoanPeriod, string theCallNumber)
+        {
+            Title = theTitle;
+            Author = theAuthor;
+            Publisher = thePublisher;
+            CopyrightYear = theCopyrightYear;
+            LoanPeriod = theLoanPeriod;
+            CallNumber = theCallNumber;
+        }
+
+        public string Author
+        {
+            // Precondition:  None
+            // Postcondition: The author has been returned
+            get
+            {
+                return _author;
+            }
+            // Precondition:  None
+            // Postcondition: The author has been set to the specified value
+            set
+            {
+                /*
+                if (value == null)
+                    _author = string.Empty;
+                else
+                    _author = value.Trim();
+                */
+
+                _author = (value == null ? string.Empty : value.Trim());
+            }
+        }
+
+        public decimal CalcLateFee(int daysLate)
+        {
+
+        const decimal LATE_FEE_RATE = 0.25m;
+        decimal chargeFee;
+
+            chargeFee = daysLate * LATE_FEE_RATE;
+            return chargeFee;
+        }
+
+        public override string ToString()
+        {
+            string NL = Environment.NewLine; // NewLine shortcut
+            string checkedOutBy; // Holds checked out message
+
+            if (IsCheckedOut())
+                checkedOutBy = $"Checked Out By: {NL}{Patron}";
+            else
+                checkedOutBy = "Not Checked Out";
+
+            return $"Title: {Title}{NL}Author: {Author}{NL}Publisher: {Publisher}{NL}" +
+                $"Copyright: {CopyrightYear}{NL}Call Number: {CallNumber}{NL}{checkedOutBy}";
+        }
+
+
+
+
     }
 }
--- CIS_200_Prog_1/LibraryItem.cs
+++ CIS_200_Prog_1/LibraryItem.cs
@@ -15,7 +15,216 @@ Section********: CIS 200 01
 
 namespace CIS_200_Prog_1
 {
-    class LibraryItem
+    public abstract class  LibraryItem
     {
+
+        private string _title;  // Backing for Title
+        private string _callNumber; //Backing for CallNumber
+        private string _publisher;  //Backing for Publisher
+        private int _copyrightYear; //Backing for Copyright Year
+        private int _loanPeriod;    //Backing for LoanPeriod
+        private bool _checkedOut;   //Backing Bool for status: Checked Out?
+
+
+        public LibraryItem(string theTitle, string thePublisher, int theCopyrightYear, 
+                           int theLoanPeriod, string theCallNumber)
+        {
+            Title = theTitle;
+            Publisher = thePublisher;
+            CopyrightYear = theCopyrightYear;
+            LoanPeriod = theLoanPeriod;
+            CallNumber = theCallNumber;
+            Patron = null;  // Item has no patron
+
+            _checkedOut = false;    // Make sure book is not checked out
+
+        }
+
+
+
+        public LibraryPatron Patron
+        {
+            // Precondition:  None
+            // Postcondition: The book's patron has been returned
+            get;
+
+            // Helper
+            // Precondition:  None
+            // Postcondition: The book's patron has been set to the specified value
+            private set;    // Auto-implement
+        }
+
+        public int LoanPeriod
+        {
+            // Precondition:  None
+            // Postcondition: The loan period has been returned
+            get
+            {
+                return _loanPeriod;
+            }
+            // Precondition:  value >= 0
+            // Postcondition: The loan period has been set to the specified value
+            set
+            {
+                if (value >= 0)
+                {
+                    _loanPeriod = value;
+                }
+                else
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(LoanPeriod)}", value, $"{nameof(LoanPeriod)} can not be a negative value");
+            }
+        }
+
+        public string Title
+        {
+            // Precondition:  None
+            // Postcondition: The title has been returned
+            get
+            {
+                return _title;
+            }
+            // Precondition:  value must not be null or empty
+            // Postcondition: The title has been set to the specified value
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(Title)}", value, $"{nameof(Title)} must not be null or empty");
+                }
+                else
+                    _title = value.Trim();
+            }
+        }
+
+        public string CallNumber
+        {
+            // Precondition:  None
+            // Postcondition: The call number has been returned
+            get
+            {
+                return _callNumber;
+            }
+
+            set
+            {
+                // Precondition:  None
+                // Postcondition: The call number has been set to the specified value
+                if (string.IsNullOrWhiteSpace(value)) // IsNullOrWhiteSpace includes tests for null, empty, or all whitespace
+                {
+                    throw new ArgumentOutOfRangeException 
+                        ($"{nameof(value)}", value, $"{nameof(value)} must not be null or empty");
+                }
+                else
+                    _callNumber = value.Trim();
+            }
+        }
+
+        public string Publisher
+        {
+            // Precondition:  None
+            // Postcondition: The publisher has been returned
+            get
+            {
+                return _publisher;
+            }
+            // Precondition:  None
+            // Postcondition: The publisher has been set to the specified value
+            set
+            {
+                /*
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(Publisher)}", value, $"{nameof(Publisher)} can not be null or empty");
+                }
+                else
+                    _publisher = value.Trim();
+                */
+
+                _publisher = (value == null ? string.Empty : value.Trim());
+            }
+        }
+
+        public int CopyrightYear
+        {
+            // Precondition:  None
+            // Postcondition: The copyright year has been returned
+            get
+            {
+                return _copyrightYear;
+            }
+            // Precondition:  value >= 0
+            // Postcondition: The copyright year has been set to the specified value
+            set
+            {
+                if (value >= 0)
+                {
+                    _copyrightYear = value;
+                }
+                else
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(CopyrightYear)}", value, $"{nameof(CopyrightYear)} must greater than or equal to zero");
+
+            }
+        }
+
+
+        //////////    METHODS    //////////
+
+
+
+        // Precondition:  thePatron != null
+        // Postcondition: The book is checked out by the specified patron
+        public void CheckOut(LibraryPatron thePatron)
+        {
+            if (thePatron != null)
+                Patron = thePatron;
+            else
+                throw new ArgumentNullException($"{nameof(thePatron)}", $"{nameof(thePatron)} must not be null");
+
+            _checkedOut = true;
+        }
+
+        // Precondition:  None
+        // Postcondition: The book is not checked out
+        public void ReturnToShelf()
+        {
+            Patron = null; // Remove previously stored reference to patron
+
+            _checkedOut = false;
+        }
+
+        // Precondition:  None
+        // Postcondition: true is returned if the book is checked out,
+        //                otherwise false is returned
+        public bool IsCheckedOut()
+        {
+            return _checkedOut;
+        }
+
+        // Precondition:  None
+        // Postcondition: A string is returned representing the libaryitem
+        public override string ToString()
+        {
+            string NL = Environment.NewLine; // NewLine shortcut
+            string checkedOutBy; // Holds checked out message
+
+            if (IsCheckedOut())
+                checkedOutBy = $"Checked Out By: {NL}{Patron}";
+            else
+                checkedOutBy = "Not Checked Out";
+
+            return $"Title: {Title}{NL}Publisher: {Publisher}{NL}" +
+                $"Copyright: {CopyrightYear}{NL}Call Number: {CallNumber}{NL}{checkedOutBy}";
+        }
+
+        // Precondition:  None
+        // Postcondition: None Abstract
+        public abstract decimal CalcLateFee();
+        
+
+
     }
 }
